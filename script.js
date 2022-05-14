3b69e3cc1e2964a53d411c3b96278ee7b6d0d210
Gustavo Ferreira <79487885+gustavoferreiradeveloper@users.noreply.github.com>
2022-05-13 22:41:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var listaFilmes = [
+  "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/84b24359960c53fc83af12127ea67e202489750fc7d0ec3fe7306ef612b2972f._UR1920,1080_RI_UX400_UY225_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/56010733019f55c70e52ef5618672df922f57a4b0812896bc25c7fd64d432429._UR1920,1080_RI_UX400_UY225_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/c5aabf71e70c51dd89da876911564b3df1929ca2770e4c4023399030f979dbc1._UR1920,1080_RI_UX400_UY225_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/0b21a40e04fb4f08268e70c9c0600e72739c37756002967fb2f1948252d2790a._UR1920,1080_RI_UX400_UY225_.jpg",
+  "https://occ-0-4134-1123.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUGACUOeLuESDoYSHjL8fNKcNYOAkGsOetqTeGX4nenyMcCUufFomyO2f6awVQQ2ijUFz1aVIh3uZ92nbmLNIavmqej57CsYbWpDVvU0I-e2qNNRvcHAjIhC23FCEUfRcdEifA.jpg?r=736",
+  "https://occ-0-4134-1123.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABRscoqK0Uw-qacmJ2IuLAaRW9jXqZi6A3mtVZlH9r7zuMA58mwRzyn09mCT1EMUR5clpXpBLJSJCZJ9zhUyxkQ4QGDLib9hSODOEmXmWORpD-L7y5qwySypfJdexe9XtnFoNmQ.jpg?r=64f",
+  "https://occ-0-4134-1123.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABWiNYU2mYv-vF31RxS_emolWkdKjFaSvOwkcP3y8jSDsjnYUnBjr38z7R_7pjFSCGlcpOKTsgUKAi9TU_0up9IAlj7wjTWQ6VWSI.jpg?r=799",
+  "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/305538cf893567f7897e06cad8eb635c75843ca2ad3a757b993773e45432d449._UR1920,1080_RI_UX400_UY225_.jpg",
+  "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/1481b8d2f9f2cc2ba62afd822f26c1013a0a060f8972b0453ef1d27fc524795e._UR1920,1080_RI_UX400_UY225_.jpg",
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write(`<img src= ${listaFilmes[i]}>`);
+}
